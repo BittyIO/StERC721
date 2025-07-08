3e9b28d69c03832e6bb35d74a1be29627d7d58a5
akashiceth <akashiceth@gmail.com>
2025-07-08 15:03:56 +0800

    add nonReentrant check

--- src/StERC721.sol
+++ src/StERC721.sol
@@ -3,10 +3,7 @@ pragma solidity 0.8.23;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import {
-    ERC721EnumerableUpgradeable,
-    ERC721Upgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import {ERC721EnumerableUpgradeable, ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
@@ -17,7 +14,12 @@ import {IStERC721} from "./interfaces/IStERC721.sol";
 import {IAssetVault} from "./interfaces/IAssetVault.sol";
 import {IAssetVaultRegistry} from "./interfaces/IAssetVaultRegistry.sol";
 
-abstract contract StERC721 is IStERC721, OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721EnumerableUpgradeable {
+abstract contract StERC721 is
+    IStERC721,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    ERC721EnumerableUpgradeable
+{
     using Clones for address;
 
     IERC721Metadata private _erc721;
@@ -39,26 +41,42 @@ abstract contract StERC721 is IStERC721, OwnableUpgradeable, ReentrancyGuardUpgr
         assetVaultRegistry = assetVaultRegistry_;
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         virtual
         override(IERC165, ERC721EnumerableUpgradeable)
         returns (bool)
     {
-        return interfaceId == type(IStERC721).interfaceId || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(IStERC721).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
-    function onERC721Received(address, address, uint256, bytes calldata) external view override returns (bytes4) {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external view override returns (bytes4) {
         require(msg.sender == address(_erc721), "StNft: nft not acceptable");
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function mint(address to_, uint256[] calldata tokenIds_) external override nonReentrant {
+    function mint(
+        address to_,
+        uint256[] calldata tokenIds_
+    ) external override nonReentrant {
         address staker_ = msg.sender;
         for (uint256 i = 0; i < tokenIds_.length; i++) {
             IAssetVault assetVault_ = assetVaultRegistry.create(staker_);
-            _erc721.safeTransferFrom(staker_, address(assetVault_), tokenIds_[i]);
+            _erc721.safeTransferFrom(
+                staker_,
+                address(assetVault_),
+                tokenIds_[i]
+            );
             _safeMint(to_, tokenIds_[i]);
         }
         emit Minted(to_, tokenIds_);
@@ -71,7 +89,10 @@ abstract contract StERC721 is IStERC721, OwnableUpgradeable, ReentrancyGuardUpgr
             tokenId_ = tokenIds_[i];
             require(staker_ == ownerOf(tokenId_), "stNft: only owner can burn");
             IAssetVault assetVault_ = assetVaultRegistry.create(staker_);
-            require(address(assetVault_) == _erc721.ownerOf(tokenId_), "stNft: invalid tokenId_");
+            require(
+                address(assetVault_) == _erc721.ownerOf(tokenId_),
+                "stNft: invalid tokenId_"
+            );
             _burn(tokenId_);
             assetVault_.withdrawERC721(staker_, address(_erc721), tokenId_);
         }
@@ -90,7 +111,9 @@ abstract contract StERC721 is IStERC721, OwnableUpgradeable, ReentrancyGuardUpgr
         return _customBaseURI;
     }
 
-    function tokenURI(uint256 tokenId_)
+    function tokenURI(
+        uint256 tokenId_
+    )
         public
         view
         override(ERC721Upgradeable, IERC721Metadata)
@@ -104,39 +127,56 @@ abstract contract StERC721 is IStERC721, OwnableUpgradeable, ReentrancyGuardUpgr
     }
 
     function contractURI() external view override returns (string memory) {
-        string memory hexAddress = Strings.toHexString(uint256(uint160(address(this))), 20);
-        return string(abi.encodePacked("https://metadata.bitty.io/eth/", hexAddress));
+        string memory hexAddress = Strings.toHexString(
+            uint256(uint160(address(this))),
+            20
+        );
+        return
+            string(
+                abi.encodePacked("https://metadata.bitty.io/eth/", hexAddress)
+            );
     }
 
-    function setDelegateCashV2(address delegate_, uint256[] calldata tokenIds_, bytes32 rights_, bool value_)
-        external
-        override
-    {
+    function setDelegateCashV2(
+        address delegate_,
+        uint256[] calldata tokenIds_,
+        bytes32 rights_,
+        bool value_
+    ) external override nonReentrant {
         address tokenOwner_;
         uint256 tokenId_;
         for (uint256 i = 0; i < tokenIds_.length; i++) {
             tokenId_ = tokenIds_[i];
             tokenOwner_ = ownerOf(tokenId_);
-            require(msg.sender == tokenOwner_, "stNft: only owner can delegate");
+            require(
+                msg.sender == tokenOwner_,
+                "stNft: only owner can delegate"
+            );
             assetVaultRegistry.create(tokenOwner_).setDelegateCashV2(
-                delegate_, address(_erc721), tokenId_, rights_, value_
+                delegate_,
+                address(_erc721),
+                tokenId_,
+                rights_,
+                value_
             );
         }
     }
 
-    function getDelegateCashForTokenV2(uint256[] calldata tokenIds_)
-        external
-        view
-        override
-        returns (address[][] memory delegates)
-    {
+    function getDelegateCashForTokenV2(
+        uint256[] calldata tokenIds_
+    ) external view override returns (address[][] memory delegates) {
         delegates = new address[][](tokenIds_.length);
         for (uint256 i = 0; i < tokenIds_.length; i++) {
-            IAssetVault assetVault_ = assetVaultRegistry.get(ownerOf(tokenIds_[i]));
+            IAssetVault assetVault_ = assetVaultRegistry.get(
+                ownerOf(tokenIds_[i])
+            );
             if (address(assetVault_) == address(0)) {
                 continue;
             }
-            delegates[i] = assetVault_.getDelegateCashForTokenV2(address(_erc721), tokenIds_[i]);
+            delegates[i] = assetVault_.getDelegateCashForTokenV2(
+                address(_erc721),
+                tokenIds_[i]
+            );
         }
     }
 }
--- src/StERC721Registry.sol
+++ src/StERC721Registry.sol
@@ -11,68 +11,118 @@ import {IStERC721} from "./interfaces/IStERC721.sol";
 import {IStERC721Registry} from "./interfaces/IStERC721Registry.sol";
 import {IAssetVaultRegistry} from "./interfaces/IAssetVaultRegistry.sol";
 
-contract StERC721Registry is OwnableUpgradeable, ReentrancyGuardUpgradeable, IStERC721Registry {
+contract StERC721Registry is
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    IStERC721Registry
+{
     string public constant namePrefix = "StNFT";
     string public constant symbolPrefix = "STNFT";
     mapping(address => address) public stERC721s;
     IAssetVaultRegistry public assetVaultRegistry;
 
-    function initialize(IAssetVaultRegistry assetVaultRegistry_) external initializer {
+    function initialize(
+        IAssetVaultRegistry assetVaultRegistry_
+    ) external initializer {
         __Ownable_init(msg.sender);
         assetVaultRegistry = assetVaultRegistry_;
     }
 
-    function createStERC721(address erc721, address stERC721Impl) external override returns (address) {
+    function createStERC721(
+        address erc721,
+        address stERC721Impl
+    ) external override nonReentrant returns (address) {
         return _createStERC721(erc721, stERC721Impl);
     }
 
-    function _createStERC721(address erc721, address stERC721Impl) internal returns (address stERC721) {
-        require(stERC721s[erc721] == address(0), "StERC721Registry: asset exist");
-        require(stERC721Impl != address(0), "StERC721Registry: impl is zero address");
+    function _createStERC721(
+        address erc721,
+        address stERC721Impl
+    ) internal returns (address stERC721) {
+        require(
+            stERC721s[erc721] == address(0),
+            "StERC721Registry: asset exist"
+        );
+        require(
+            stERC721Impl != address(0),
+            "StERC721Registry: impl is zero address"
+        );
         stERC721 = _createProxy(erc721, stERC721Impl);
         stERC721s[erc721] = stERC721;
     }
 
-    function _createProxy(address erc721, address stERC721Impl) internal returns (address stERC721) {
+    function _createProxy(
+        address erc721,
+        address stERC721Impl
+    ) internal returns (address stERC721) {
         bytes memory initParams = _buildInitParams(erc721);
-        stERC721 = address(new TransparentUpgradeableProxy(stERC721Impl, address(this), initParams));
-    }
-
-    function _buildInitParams(address erc721) internal view returns (bytes memory initParams) {
-        string memory nftSymbol = IERC721Metadata(erc721).symbol();
-        string memory bNftName = string(abi.encodePacked(namePrefix, " ", nftSymbol));
-        string memory bNftSymbol = string(abi.encodePacked(symbolPrefix, nftSymbol));
-        initParams = abi.encodeWithSelector(
-            IStERC721.initialize.selector, IERC721Metadata(erc721), assetVaultRegistry, bNftName, bNftSymbol
+        stERC721 = address(
+            new TransparentUpgradeableProxy(
+                stERC721Impl,
+                address(this),
+                initParams
+            )
         );
     }
 
-    function batchCreateStERC721(address[] calldata erc721s, address stERC721Impl)
-        external
-        override
-        returns (address[] memory stERC721s_)
-    {
+    function _buildInitParams(
+        address erc721
+    ) internal view returns (bytes memory initParams) {
+        string memory nftSymbol = IERC721Metadata(erc721).symbol();
+        string memory bNftName = string(
+            abi.encodePacked(namePrefix, " ", nftSymbol)
+        );
+        string memory bNftSymbol = string(
+            abi.encodePacked(symbolPrefix, nftSymbol)
+        );
+        initParams = abi.encodeWithSelector(
+            IStERC721.initialize.selector,
+            IERC721Metadata(erc721),
+            assetVaultRegistry,
+            bNftName,
+            bNftSymbol
+        );
+    }
+
+    function batchCreateStERC721(
+        address[] calldata erc721s,
+        address stERC721Impl
+    ) external override nonReentrant returns (address[] memory stERC721s_) {
         for (uint256 i = 0; i < erc721s.length; i++) {
             stERC721s_[i] = _createStERC721(erc721s[i], stERC721Impl);
         }
     }
 
-    function upgradeStERC721(address erc721, address stERC721Impl, bytes calldata encodedCallData) external override {
+    function upgradeStERC721(
+        address erc721,
+        address stERC721Impl,
+        bytes calldata encodedCallData
+    ) external override nonReentrant {
         _upgradeStERC721(erc721, stERC721Impl, encodedCallData);
     }
 
-    function _upgradeStERC721(address erc721, address stERC721Impl, bytes memory encodedCallData) internal {
+    function _upgradeStERC721(
+        address erc721,
+        address stERC721Impl,
+        bytes memory encodedCallData
+    ) internal {
         address stERC721Proxy = stERC721s[erc721];
-        require(stERC721Proxy != address(0), "StERC721Registry: asset nonexist");
+        require(
+            stERC721Proxy != address(0),
+            "StERC721Registry: asset nonexist"
+        );
 
-        ITransparentUpgradeableProxy proxy = ITransparentUpgradeableProxy(payable(stERC721Proxy));
+        ITransparentUpgradeableProxy proxy = ITransparentUpgradeableProxy(
+            payable(stERC721Proxy)
+        );
         proxy.upgradeToAndCall(stERC721Impl, encodedCallData);
     }
 
-    function batchUpgradeStERC721(address[] calldata erc721s, address stERC721Impl, bytes[] calldata encodedCallDatas)
-        external
-        override
-    {
+    function batchUpgradeStERC721(
+        address[] calldata erc721s,
+        address stERC721Impl,
+        bytes[] calldata encodedCallDatas
+    ) external override nonReentrant {
         for (uint256 i = 0; i < erc721s.length; i++) {
             _upgradeStERC721(erc721s[i], stERC721Impl, encodedCallDatas[i]);
         }
