fc33f9fc11c776ec49be91feb933ef12afd55b31
akashiceth <akashiceth@gmail.com>
2025-07-08 20:56:40 +0800

    change solidity 0.8.23 -> 0.8.29

--- test/mock/MintableERC1155.sol
+++ test/mock/MintableERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.23;
+pragma solidity 0.8.29;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
