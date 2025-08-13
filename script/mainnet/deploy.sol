4de19ee21a2d4a3b8071f20254ae8658e169eedd
akashiceth <akashiceth@gmail.com>
2025-08-13 18:46:41 +0800

    deploy mainnet

---
+++ script/mainnet/deploy.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.29;
+
+import {console2} from "forge-std/console2.sol";
+import {Script} from "forge-std/Script.sol";
+import {Upgrades} from "@openzeppelin-foundry-upgrades/Upgrades.sol";
+import {Options} from "@openzeppelin-foundry-upgrades/Options.sol";
+
+import {AssetVaultRegistry} from "../../src/AssetVaultRegistry.sol";
+import {IAssetVaultRegistry} from "../../src/interfaces/IAssetVaultRegistry.sol";
+import {StERC721Registry} from "../../src/StERC721Registry.sol";
+import {IStERC721Registry} from "../../src/interfaces/IStERC721Registry.sol";
+import {IStERC721} from "../../src/interfaces/IStERC721.sol";
+import {IAssetVault} from "../../src/interfaces/IAssetVault.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract DeployScript is Script {
+    address public constant INITIAL_OWNER_ADDRESS_FOR_PROXY_ADMIN = 0x75b38661D59706aD533bA8905502dd19957dc228;
+    address public constant DELEGATION_REGISTRY_V2 = 0x00000000000000447e69651d841bD8D104Bed493;
+
+    function run() public {
+        vm.createSelectFork("mainnet");
+        vm.startBroadcast(vm.envUint("MAINNET_PRIVATE_KEY"));
+        Options memory opts;
+        address assetVaultImpl = Upgrades.deployImplementation("AssetVault.sol", opts);
+        address assetVaultRegistry = Upgrades.deployTransparentProxy(
+            "AssetVaultRegistry.sol",
+            INITIAL_OWNER_ADDRESS_FOR_PROXY_ADMIN,
+            abi.encodeCall(IAssetVaultRegistry.initialize, (assetVaultImpl, DELEGATION_REGISTRY_V2))
+        );
+        address stERC721Registry = Upgrades.deployTransparentProxy(
+            "StERC721Registry.sol",
+            INITIAL_OWNER_ADDRESS_FOR_PROXY_ADMIN,
+            abi.encodeCall(IStERC721Registry.initialize, ("eth", IAssetVaultRegistry(assetVaultRegistry)))
+        );
+        OwnableUpgradeable(assetVaultRegistry).transferOwnership(stERC721Registry);
+        address stERC721Impl = Upgrades.deployImplementation("StERC721.sol", opts);
+        vm.stopBroadcast();
+        console2.log("assetVaultImpl", assetVaultImpl);
+        console2.log("stERC721Impl", stERC721Impl);
+        console2.log("assetVaultRegistry", assetVaultRegistry);
+        console2.log("stERC721Registry", stERC721Registry);
+    }
+}
